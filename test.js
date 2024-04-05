45d93d73dcd6956ee0c5d718bce82cd501310158
Sherin T S. <sherin.ts@cloudium.io>
2024-04-05 11:31:10 +0530

    first

--- test.js
+++ test.js
@@ -1,50 +1,59 @@
-function getComputerChoice() {
-    const choices = ["rock", "paper", "scissors"];
-    return choices[Math.floor(Math.random() * choices.length)];
-}
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        return "tie";
-    } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        return "win";
-    } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        return "win";
-    } else if (playerSelection == "paper" && computerSelection == "rock") {
-        return "win";
-    } else {
-        return "lose";
+function computerChoice() {
+    let choice = ['rock', 'papper', 'scissor']
+    return choice[Math.floor(Math.random() * choice.length)];
+}
+function playgame() {
+    w = 0;
+    l = 0;
+    t = 0;
+    result = "";
+    for (let i = 0; i < 5; i++) {
+        let computerCh = computerChoice();
+        let playerchoice = prompt("Enter Your Try").toLowerCase();
+        if (playerchoice === 'rock' || playerchoice === 'papper' || playerchoice === 'scissor') {
+            result = playround(computerCh, playerchoice)
+            console.log(result)
+            if (result === "Player win") {
+                w++;
+
+            }
+            else if (result === "Computer win") {
+                l++;
+
+
+            }
+            else if (result === "Tie") {
+                t++;
+            }
+        }else{i--}
+    }
+    if (w > l) {
+        console.log("You win")
+    }
+    else if (l > w) {
+        console.log("You Lose")
+    }
+    else if (w == l) {
+        console.log("Match Tie")
+    }
+}
+playgame();
+function playround(computerCh, playerchoice) {
+    if (playerchoice == computerCh) {
+        return ("Tie")
+    }
+    else if (playerchoice == 'rock' && computerCh == 'scissor') {
+        return ("Player win")
+    }
+    else if (playerchoice == 'papper' && computerCh == 'rock') {
+        return ("Player win")
+    }
+    else if (playerchoice == 'scissor' && computerCh == 'papper') {
+        return ("Player win")
+    }
+    else {
+        return ("Computer win")
     }
 }
 
-function playGame() {
-    let count = 0;
-    let c = 0;
-    let p = 0;
-    for (i = 0; i < 5; i++) {
-
-        const playerSelection = prompt("Enter You Choice").toLowerCase();
-        const computerSelection = getComputerChoice();
-        const result = playRound(playerSelection, computerSelection);
-
-        console.log(result);
-        if (result == "tie") {
-            console.log("tie");
-        } else if (result == "win") {
-            console.log("you win");
-            p = p + 1;
-        } else if (result == "lose") {
-            console.log("you lose");
-            c = c + 1;
-        }
-    }
-    if (p > c) {
-        console.log("Winner")
-    }else if(p<c) {
-        console.log("Loser")
-    }else{ console.log("Tie")}
-}
-playGame();
-// const playerSelection = "rock";
-// const computerSelection = getComputerChoice();
-// console.log(playRound(playerSelection, computerSelection));
